630728cb1c10d32fe560fcba2e502cca0fd3a768
frankcsliu <frankcsliu@frankcslius-Mac-mini.local>
2018-12-11 00:04:42 +0800

    R語法檔及資料檔

--- 07炒與炸_模型的建立與分析結果詮釋/7.4_迴歸模型的比較製圖與製表.R
+++ 07炒與炸_模型的建立與分析結果詮釋/7.4_迴歸模型的比較製圖與製表.R
@@ -54,60 +54,44 @@ plot_models(md03, md04, colors = "gs") # 使用灰階色系（greyscale）
 
 ## 陽春版的表格
 library(sjPlot)
-sjt.glm(md01) 
+tab_model(md01) 
 
 ## 專業版的表格
 library(sjPlot)
-sjt.glm(md03,
+tab_model(md03, 
         # 設定要顯示的資訊
-        exp.coef=FALSE, # 不要將迴歸係數還原為勝算
-        show.header = TRUE, # 顯示模型標題列
-        show.ci = TRUE, # 顯示信賴區間
-        separate.ci.col = TRUE,  # 以獨立欄位顯示信賴區間
         show.se = TRUE, # 顯示標準誤
         show.aic = TRUE, # 顯示AIC 
-        show.loglik = TRUE, # 顯示 -2*Log-Likelihood
         show.r2 = TRUE, # 顯示 (pseudo) R-square
-        p.numeric=F, #切換為以星號表示顯著程度
+        p.style="asterisk", # 以星號表示顯著程度
+        collapse.ci = FALSE,  # 以獨立欄位顯示信賴區間
         
         # 設定小數點的位數
-        digits.est=3,   
-        digits.p=3,
-        digits.se=3,
-        digits.ci=3,
-        digits.summary= 2,
+        digits=3, # 所有估計值的小數點位數
+        digits.p=3, # p值的小數點位數
         
         # 線的樣式設計
         CSS=list(css.topborder="border-top:1px solid black;"), 
 
         # 欄位重新命名
-        string.est ="迴歸係數",
-        string.se ="標準誤 (S.E.)",
-        string.p="顯著水準 (C.I.)", 
+        string.pred = "解釋變數",
         string.ci = "信賴區間",
-        string.obs = "觀察值（N）", 
-        string.interc = "截距", 
-        string.dv =  "模型一", 
-        string.pred = "解釋變數"
+        string.se ="標準誤 (S.E.)",
+        string.p="顯著水準 (C.I.)"
         # file="analysisResult.html" #另存新檔
         )
 
+
 ## 以sjt.glm()製表來比較模型
-sjt.glm(md03, md04,
-        show.aic=TRUE, 
-        show.loglik=TRUE,
-        show.ci=FALSE,
-        show.se=TRUE,
-        show.r2 =FALSE,
-        show.header = TRUE,
-        digits.p=3, 
-        digits.se=3,
-        digits.est=3,
-        CSS=list(css.topborder="border-top:1px solid black;"), 
-        exp.coef=FALSE,
-        string.est ="Reg. Coef.",
-        string.se ="Std. Error"
-        )
+tab_model(md03, md04,
+          show.aic=TRUE, 
+          show.ci=FALSE,
+          show.se=TRUE,
+          show.r2 =FALSE,
+          digits.p=3, 
+          digits=3,
+          CSS=list(css.topborder="border-top:1px solid black;")
+          )
 
 ### 補充盒子： 為多元及有序勝算對數分析結果製表
 library(stargazer)
