6ca6b2085c0e1c9ffaf82f8cc7c732d433d444ff
Frank Liu <frankcsliu@Frank-MacBook-Pro.local>
2020-10-23 22:03:56 +0800

    R語法檔及資料檔

--- 05備料_變數的編碼與描述/5.3_變數的描述製表與製圖.R
+++ 05備料_變數的編碼與描述/5.3_變數的描述製表與製圖.R
@@ -18,18 +18,19 @@ library(sjmisc)
 frq(tscs2013$v65)
 frq(tscs2013$v65, weights = tscs2013$wr) 
 
-
 ## 清除無效值、重新編碼、排序及上標籤
 tscs2013$v65r <- rec(tscs2013$v65, 
                      rec="94:99=NA; else=copy",  
                      as.num = F)
-tscs2013$v65r <- rec(tscs2013$v65r, rec="rev", 
+frq(tscs2013$v65r)
+tscs2013$v65rv <- rec(tscs2013$v65r, rec="rev", 
                      var.label="how serious the issue is", 
                      val.labels = c("not at all (1)", 
                                     "not very serious (2)", 
                                     "serious (3)", 
                                     "very serious (4)"))
-frq(tscs2013$v65r, weights = tscs2013$wr) 
+frq(tscs2013$v65rv)
+# frq(tscs2013$v65rv, weights = tscs2013$wr) 
 save(tscs2013, file="tscs2013r.rda")  #把含有新增變數的資料檔另存在工作資料夾
 
 # psych::describe()
@@ -44,6 +45,10 @@ descr(tscs2013$age)
 ## 使用sjPlot為單一變數製圖
 library(sjPlot)
 sjmisc::frq(tscs2013$v65r)
+
+# Mac使用者需設定字體才能正常顯示標籤中的中文字
+# sjPlot::set_theme(sjPlot::set_theme(theme.font="PingFang TC"))  
+
 plot_frq(tscs2013$v65r, 
         weight.by = tscs2013$wr, wrap.title=30)
 
@@ -52,8 +57,8 @@ plot_frq(tscs2013$v65r,
 library(sjPlot)
 library(sjlabelled)
 frq(tscs2013$sex)
-tscs2013$sex <- set_label(tscs2013$sex, label="sex")  # 重設變數標籤
-tscs2013$sex <- set_labels(tscs2013$sex, labels= c("female", "male")) #重設選項標籤
+tscs2013$sex <- set_label(tscs2013$sex, label="sex")  # 變更變數標籤
+tscs2013$sex <- set_labels(tscs2013$sex, labels= c("female", "male")) # 變更選項標籤
 plot_grpfrq(tscs2013$v65r, tscs2013$sex)  # 橫向比較
 plot_grpfrq(tscs2013$v65r, tscs2013$sex, bar.pos = "stack")  # 堆疊
 
