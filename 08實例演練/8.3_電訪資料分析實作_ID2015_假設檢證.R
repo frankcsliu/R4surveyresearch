10268f3c22cb06cd33e7a5fea713c25107ae74ca
Frank Liu <chengshanl@hotmail.com>
2021-05-22 15:01:41 +0800

    R語法檔及資料檔

--- 08實例演練/8.3_電訪資料分析實作_ID2015_假設檢證.R
+++ 08實例演練/8.3_電訪資料分析實作_ID2015_假設檢證.R
@@ -209,8 +209,6 @@ id15$generation <- set_labels(id15$generation,
                                 c("第一世代(>=84)","第二世代(62~83)",
                                   "第三世代(47~61)","第四世代(37~46)",
                                   "第五世代(27~36)","第六世代(<=26)"))
-
-
 frq(id15$generation)
 
 id15$gen.1 <- set_label(id15$gen.1, "第一世代(>=84)")
@@ -220,22 +218,3 @@ id15$gen.4 <- set_label(id15$gen.4, "第四世代(37~46)")
 id15$gen.5 <- set_label(id15$gen.5, "第五世代(27~36)")
 id15$gen.6 <- set_label(id15$gen.6, "第六世代(<=26)")
 frq(id15$gen.3)
-
-# 試將V16~V20加總製作成指標，先作初步分析：
-load("../id15.rda")
-tmp <- subset(id15, select=c(V16:V20))
-tmp <- as.data.frame(na.omit(tmp))
-sjPlot::sjt.itemanalysis(tmp) 
-
-# 因 sjstats 套件功能調整，信度測量可以使用 performance::item_reliability()
-# https://cran.r-project.org/web/packages/sjstats/news/news.html
-
-# 這兩行已作廢
-# library(sjstats)
-# reliab_test(tmp, scale.items = T)
-
-# 新的做法：
-# install.packagez("performance")
-library(performance)
-item_reliability(tmp)
-
